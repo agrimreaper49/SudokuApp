f8745a525fbbe822090b59da5231cff12ba240d5
Agrim Sharma <76flyershd@gmail.com>
2021-09-30 09:08:23 -0400

    working branch clean

--- app/src/main/java/com/example/sudokuapp/SplashScreen.kt
+++ app/src/main/java/com/example/sudokuapp/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.sudokuapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
