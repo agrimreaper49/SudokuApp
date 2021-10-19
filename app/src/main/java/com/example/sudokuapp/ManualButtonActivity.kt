4620a8fd9bd3eb886cdb0c95eceae3d543f0a9dc
Agrim Sharma <76flyershd@gmail.com>
2021-10-19 09:28:23 -0400

    added manual button

---
+++ app/src/main/java/com/example/sudokuapp/ManualButtonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sudokuapp
+
+import android.app.AppComponentFactory
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ManualButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manualbutton)
+    }
+}
