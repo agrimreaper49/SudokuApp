f47e87524eaa383049b01e254ef006db2f3e8344
Austin Tran <austin4tran@gmail.com>
2021-10-19 09:41:31 -0400

    Button on main page added

--- app/src/main/java/com/example/sudokuapp/MainActivity.kt
+++ app/src/main/java/com/example/sudokuapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sudokuapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val scanButton = findViewById<Button>(R.id.button2)
+        scanButton.setOnClickListener{
+            val cameraIntent = Intent(this, CameraActivity::class.java)
+            startActivity(cameraIntent)
+        }
 }
