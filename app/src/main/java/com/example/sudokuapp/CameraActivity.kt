884b4e35a0d9195886b3a4a5a005f72078d1b50c
Austin Tran <austin4tran@gmail.com>
2021-10-25 09:02:34 -0400

    Setup imageView in xml file

--- app/src/main/java/com/example/sudokuapp/CameraActivity.kt
+++ app/src/main/java/com/example/sudokuapp/CameraActivity.kt
@@ -1,11 +1,31 @@
 package com.example.sudokuapp
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 
 class CameraActivity : AppCompatActivity() {
+    private val cameraRequest = 1888
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        title = "KotlinApp"
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
+        imageView = findViewById(R.id.imageView)
+        val photoButton: Button = findViewById(R.id.photoButton)
+        photoButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
     }
 }
